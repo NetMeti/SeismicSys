8dde30b37c423cc7e09e52a9323a168654b17e78
SHazdeApp <shazdeapp@gmail.com>
2025-10-15 08:53:47 +0330

    new game
    Signed-off-by: SHazdeApp <shazdeapp@gmail.com>

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1760505724,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/table-sheet0.png",
+		"images/ui_countdown.png",
+		"images/destinations-sheet0.png",
+		"images/ui_next-sheet0.png",
+		"images/cup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
